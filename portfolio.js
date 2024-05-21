b2b9c04d4bbaa4d5e0dc368c2eb7317d339db01b
ansel-lee <ansellee23@gmail.com>
2024-05-22 00:27:54 +0800

    LOADS of changes
    Added new writeups.
    Added the More... functionality. Chose not to pop-out in case people don't have Javascript (in that case it will just show).
    Refactored some of the navigation JS. Found making it a button unnecessary.
    Toyed around with Navigation CSS. Neatened and removed a few extra lines.
    
    Starting up on my side-by-side container. Hopefully I can finish it in time.
    
    I can't believe this is all I did in 6 hours. Have fun!

--- portfolio.js
+++ portfolio.js
@@ -190,6 +190,7 @@ document.addEventListener('DOMContentLoaded', () => {
         nav_watcher.update(checkInView(nav));
     });
 
+    /*
     // Apply smooth scrolling to navlist
     let nav_lis = document.getElementsByClassName('nav-li')
     for (let i = 0; i < nav_lis.length; i++) {
@@ -206,8 +207,7 @@ document.addEventListener('DOMContentLoaded', () => {
             nav_lis[i].appendChild(buttonElm);
             child.remove();
         }
-    }
-
+    }*/
     shuffle_colors();
 })
 
@@ -215,8 +215,44 @@ function shownavbar() {
     document.getElementsByTagName('header')[0].classList.toggle('hidenav')
 }
 
+function showmore(e) {
+    e.currentTarget.classList.add("nodisp");
+    e.currentTarget.nextElementSibling.classList.remove("nodisp");
+}
+
 
 function init() {
     document.getElementsByTagName('header')[0].classList.add('hidenav')
     document.getElementById("navhintfloat").children[0].addEventListener("click", shownavbar)
+
+    // bind nav list to function instead of link
+    let linksparent = document.getElementsByClassName("nav-list")[0]
+    let i = linksparent.children.length
+    console.log(linksparent)
+    while (i--) {
+        let currElm = linksparent.children[i].children[0];
+        if (currElm.tagName != "A") { continue; }
+
+        console.log(currElm.href.split('#')[1]);
+        // add event listener does not cancel function, so onclick better
+        currElm.onclick = () => {
+            smoothTo(currElm.href.split('#')[1])
+            window.target = currElm.href.split("#")[1]
+            return false;
+        };
+    }
+
+    // show more with more button (and hide text using js so noscript works)
+    let morebuttons = document.getElementsByClassName("morebutton");
+    let j = morebuttons.length
+    while (j--) {
+        let currElm = morebuttons[j];
+        currElm.classList.remove("nodisp")
+        currElm.tabIndex = 0; // its weird, dont ask me why not tabindex
+        currElm.addEventListener("click", showmore);
+        currElm.addEventListener("keyup", (e) => {
+            e && e.keyCode == 13 ? showmore(e) : 0;
+        })
+        currElm.nextElementSibling.classList.add("nodisp");
+    }
 }
